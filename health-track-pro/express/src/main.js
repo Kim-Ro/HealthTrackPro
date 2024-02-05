5b5b72adb06d1db6da28617ac265deac67064f76
SoulCake82 <153026773+SoulCake82@users.noreply.github.com>
2024-02-05 22:16:34 +0100

    Added code to create new profile
    - profile schema
    - route for new profile, including calculation of age based on date of birth

--- health-track-pro/express/src/main.js
+++ health-track-pro/express/src/main.js
@@ -7,6 +7,7 @@ import express from 'express';
 import * as path from 'path';
 import cors from 'cors';
 import checkup from "./models/checkup-model.js"
+import Profile from "./models/profile-model.js"
 const app = express();
 app.use(cors())
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
@@ -15,13 +16,41 @@ app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 
 app.get('/api', (req, res) => {
-  checkup.find({"age.min":{$lte:34}, "age.max":{$gte:34}})
-  .then((result) => {
-    res.send(result);
-  })
+  checkup.find({ "age.min": { $lte: 30 }, "age.max": { $gte: 30 } })
+    .then((result) => {
+      res.send(result);
+    })
 });
 
+//// create a new profile, calculate age, add all data to database. IT WORKS //////
+function getAge(year, month, day) {
+  const birth = new Date(year, month - 1, day)
+  const now = new Date()
+  const diff = new Date(now.valueOf() - birth.valueOf())
+  const age = Math.abs(diff.getFullYear() - 1970)
+  return age
+}
 
+app.get("/api/newProfile", async (req, res) => {
+  try {
+    const { name, sex, dateOfBirth } = req.query;   ///see how data arrives from frontend. Maybe change to req.body if necessary /////
+    const [year, month, day] = dateOfBirth.split("-");
+    const age = getAge(year, month, day);
+    const newProfile = new Profile({
+      name: name,
+      sex: sex,
+      dateOfBirth: dateOfBirth,
+      age: age
+    })
+    await newProfile.save()
+      .then(res.send("New profile created!"))
+  }
+  catch (err) {
+    console.log("Something went wrong", err)
+    res.send("Something went wrong!")
+  }
+
+})
 
 
 
