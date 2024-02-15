eb099657ca0a38d49259df0c2e6a0e6aa99a424e
Tiana Barnekow <150627774+tialuci@users.noreply.github.com>
2024-02-15 18:45:40 +0100

    finished routing in main.ts, updated/added readme, removed express folger without typescript
    Removed Express folder so that there is no confusion with the typescript express.
    Updated Readme, added Readme in express-ts to explain the routing as is
    
    minor improvements

--- health-track-pro/express-ts/src/main.ts
+++ health-track-pro/express-ts/src/main.ts
@@ -6,13 +6,12 @@ import User from "./models/user-model";
 const app = express();
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //// workspace begin //////
 
-//// CREATE: create a new user with first profile, calculate age, add eligible checkups //////
-
+//functions
 const getAge = function (dateOfBirth) {
   const [year, month, day] = dateOfBirth.split('-');
   const birth = new Date(year, month - 1, day);
@@ -20,7 +19,7 @@ const getAge = function (dateOfBirth) {
   const diff = new Date(now.valueOf() - birth.valueOf());
   const age = Math.abs(diff.getFullYear() - 1970);
   return age;
-}
+};
 
 const createUser = async function (userAuthID, name, sex, dateOfBirth, age) {
   const user = new User({
@@ -32,48 +31,38 @@ const createUser = async function (userAuthID, name, sex, dateOfBirth, age) {
       dateOfBirth: dateOfBirth,
       age: age
     }
-  })
+  });
   await user.save();
   return user;
-}
+};
 
-const getCheckups = async function (userAuthID, profileID) {
-  const user = await User.findOne({ "userAuthID": userAuthID });
-  const profile = user.profiles.id(profileID);
-  const age = profile.age;
-  const sex = profile.sex;
-  const checkups = await CheckUp.find({ "sex": sex, "age.min": { $lte: age }, "age.max": { $not: { $lt: age } } });
-  for (const element of checkups) {
-    profile.availableCheckups.push(element._id);
-  }
-  await user.save();
-}
+const getCheckups = async function (profile) {
+  const checkups = await CheckUp.find({ "sex": profile.sex, "age.min": { $lte: profile.age }, "age.max": { $not: { $lt: profile.age } } });
+  return checkups;
+};
 
-const newUser = async function (userAuthID, name, sex, dateOfBirth) {
-  const age = getAge(dateOfBirth);
-  const user = await createUser(userAuthID, name, sex, dateOfBirth, age);
-  const userID = user._id;
-  const profileID = user.profiles[0]._id;
-  await getCheckups(userAuthID, profileID);
-  await user.save()
-}
 
-// Create new user, including first profile + available checkups
-
+// POST request to create new databankuser + first profile
 app.post("/api/newUser", async (req, res) => {
   try {
     const { userAuthID, name, sex, dateOfBirth } = req.body;
-    await newUser(userAuthID, name, sex, dateOfBirth);
+    const age = getAge(dateOfBirth);
+    const user = await createUser(userAuthID, name, sex, dateOfBirth, age);
+    const profile = user.profiles[0];
+    const checkups = await getCheckups(profile);
+    for (const element of checkups) {
+      user.profiles.id(profile._id).availableCheckups.push(element._id);
+    }
+    await user.save();
     res.send({ message: "New user created!" });
   }
   catch (err) {
     console.log("Something went wrong", err);
     res.send({ message: "Something went wrong." });
   }
-})
+});
 
-//Look at user with all existing profiles + automatic update
-
+// GET request to fetch user data with all existing profiles
 app.get("/api/user/:userAuthID/profiles", async (req, res) => {
   try {
     const { userAuthID } = req.params;
@@ -84,39 +73,37 @@ app.get("/api/user/:userAuthID/profiles", async (req, res) => {
       if (profile.age !== age) {
         profile.age = age;
         profile.availableCheckups = [];
-        const checkups = await CheckUp.find({ "sex": profile.sex, "age.min": { $lte: profile.age }, "age.max": { $not: { $lt: profile.age } } });
+        const checkups = await getCheckups(profile);
         for (const element of checkups) {
           profile.availableCheckups.push(element._id);
         }
       }
     }
-    await user.save()
-    res.send(user.profiles)
+    await user.save();
+    res.send(user);
   }
   catch (err) {
     console.log("Something went wrong", err);
     res.send({ message: "Something went wrong." });
   }
-})
+});
 
-//Fetch specific profile from user account 
-
+//GET request to fetch data for specific profile
 app.get("/api/user/:userAuthID/profiles/:profileID", async (req, res) => {
   try {
     const { userAuthID, profileID } = req.params;
     const user = await User.findOne({ "userAuthID": userAuthID });
     const profile = user.profiles.id(profileID);
     await user.populate("profiles.availableCheckups");
-    res.send(profile)
+    res.send(profile);
   }
   catch (err) {
     console.log("Something went wrong", err);
     res.send({ message: "Something went wrong." });
   }
-})
+});
 
-//Create a new profile in user account
-
+//POST request to create new profile in databank user
 app.post("/api/user/:userAuthID/profiles", async (req, res) => {
   try {
     const { userAuthID } = req.params;
@@ -129,8 +116,8 @@ app.post("/api/user/:userAuthID/profiles", async (req, res) => {
       dateOfBirth: dateOfBirth,
       age: age,
       availableCheckups: []
-    }
-    const checkups = await CheckUp.find({ "sex": sex, "age.min": { $lte: age }, "age.max": { $not: { $lt: age } } });
+    };
+    const checkups = await getCheckups(newProfile);
     for (const element of checkups) {
       newProfile.availableCheckups.push(element._id);
     }
@@ -139,88 +126,88 @@ app.post("/api/user/:userAuthID/profiles", async (req, res) => {
     res.send({ message: "New profile created!" });
   }
   catch (err) {
-    console.log("Something went wrong.", err)
-    res.send({ message: "Something went wrong." })
+    console.log("Something went wrong.", err);
+    res.send({ message: "Something went wrong." });
   }
-})
+});
 
 
-// update user
-app.patch("/api/user/:userAuthID", async (req, res) => {
+//PUT request to update databank user name (does NOT update user account from registration!)
+app.put("/api/user/:userAuthID", async (req, res) => {
   try {
     const { userAuthID } = req.params;
     const { newUserName } = req.body;
     const user = await User.findOne({ "userAuthID": userAuthID });
     user.name = newUserName;
-    await user.save()
-    res.send({ message: "User updated!" })
+    await user.save();
+    res.send({ message: "User updated!" });
   }
   catch (err) {
-    console.log("Something went wrong.", err)
-    res.send({ message: "Something went wrong." })
+    console.log("Something went wrong.", err);
+    res.send({ message: "Something went wrong." });
   }
-})
+});
 
-// update profile
-app.patch("/api/user/:userAuthID/profiles/:profileID", async (req, res) => {
+//PUT request to update data for specific profile
+app.put("/api/user/:userAuthID/profiles/:profileID", async (req, res) => {
   try {
     const { userAuthID, profileID } = req.params;
     const { newName, newSex, newDateOfBirth } = req.body;
     const user = await User.findOne({ "userAuthID": userAuthID });
     const profile = user.profiles.id(profileID);
     if (profile.name !== newName) {
-      profile.name = newName
+      profile.name = newName;
     };
     if (profile.sex !== newSex) {
-      profile.name = newSex
+      profile.name = newSex;
     };
     if (profile.dateOfBirth !== newDateOfBirth) {
-      profile.dateOfBirth = newDateOfBirth
-      const age = getAge(newDateOfBirth)
-      profile.age = age
+      profile.dateOfBirth = newDateOfBirth;
+      const age = getAge(newDateOfBirth);
+      profile.age = age;
     };
     profile.availableCheckups = [];
-    const checkups = await CheckUp.find({ "sex": profile.sex, "age.min": { $lte: profile.age }, "age.max": { $not: { $lt: profile.age } } });
+    const checkups = await getCheckups(profile);
     for (const element of checkups) {
       profile.availableCheckups.push(element._id);
     }
-    await user.save()
-    res.send({ message: "Profile updated!" })
+    await user.save();
+    res.send({ message: "Profile updated!" });
   }
   catch (err) {
-    console.log("Something went wrong.", err)
-    res.send({ message: "Something went wrong." })
+    console.log("Something went wrong.", err);
+    res.send({ message: "Something went wrong." });
   }
-})
+});
 
-//delete user
+//DELETE request to delete databank user with all existing profiles. Does NOT delete user account from registration!
 app.delete("/api/user/:userAuthID", async (req, res) => {
   try {
     const { userAuthID } = req.params;
     const user = await User.findOne({ "userAuthID": userAuthID });
     await User.findByIdAndDelete(user._id);
-    res.send({ message: "Your User was deleted, it is no more!" })
+    res.send({ message: "Your User was deleted, it is no more!" });
   }
   catch (err) {
-    console.log("Something went wrong.", err)
-    res.send({ message: "Something went wrong!" })
+    console.log("Something went wrong.", err);
+    res.send({ message: "Something went wrong!" });
   }
-})
+});
 
-//delete selected profile
+//DELETE request to delete a specific profile
 app.delete("/api/user/:userAuthID/profiles/:profileID", async (req, res) => {
   try {
     const { userAuthID, profileID } = req.params;
     const user = await User.findOne({ "userAuthID": userAuthID });
-    await user.profiles.id(profileID).deleteOne()
-    await user.save()
-    res.send({ message: "Selected Profile was deleted" })
+    await user.profiles.id(profileID).deleteOne();
+    await user.save();
+    res.send({ message: "Profile deleted." });
   }
   catch (err) {
-    console.log("Something went wrong.", err)
-    res.send({ message: "Something went wrong!" })
+    console.log("Something went wrong.", err);
+    res.send({ message: "Something went wrong!" });
   }
-})
+});
 
 
 ///// workspace end //////
