2a5015845aeac1454570a2146ba884dfa549e212
jpluedtke <jp.luedtke@gmail.com>
2024-02-15 13:32:33 +0100

    Add files via upload

--- health-track-pro/react/src/components/AppBar.js
+++ health-track-pro/react/src/components/AppBar.js
@@ -5,9 +5,9 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { ReactSVG } from "react-svg";
 import Logo from "../images/logo-placeholder.svg"
+import Button from '@mui/material/Button';
 
-
-export default function MyAppBar() {
+export default function MyAppBar({ isAuthenticated, onLogin, onLogout }) {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
@@ -16,7 +16,12 @@ export default function MyAppBar() {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1, ml: 2 }}>
                         Health Track Pro
                     </Typography>
-                </Toolbar>
+                    {!isAuthenticated ? (
+                    <Button color="inherit" onClick={onLogin}>Login</Button>
+                    ) : (
+                    <Button color="inherit" onClick={onLogout}>Logout</Button>
+                    )}
+                  </Toolbar>
             </AppBar>
         </Box>
     );
